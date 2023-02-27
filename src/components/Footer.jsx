e0104d57ff8bf622838ada95f646cbe432b001bf
KiarieLinus <kiarielinus@gmail.com>
2023-02-27 20:11:43 +0300

    Final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,27 @@
+import twitter from '../assets/Twitter.png'
+import instagram from '../assets/Instagram.png'
+import github from '../assets/Github.png'
+import linkedin from '../assets/Linkedin.png'
+
 export default function Footer(){
-    
+    return(
+        <footer>
+            <a href='https://twitter.com/KiarieLinus' target='_blank'>
+                <img src={twitter}/>
+            </a>
+
+            <a href='https://www.instagram.com/kiarie.linus/' target='_blank'>
+                <img src={instagram}/>
+            </a>
+
+            <a href='https://www.linkedin.com/in/linus-kiarie/' target='_blank'>
+                <img src={linkedin}/>
+            </a>
+
+            <a href='https://github.com/KiarieLinus' target='_blank'>
+                <img src={github}/>
+            </a>
+
+        </footer>
+    )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,4 +1,5 @@
 import profile from '../assets/profile.png';
+import mail from '../assets/Mail.svg';
 
 export default function Info() {
     return (
@@ -8,16 +9,10 @@ export default function Info() {
             <h3>Frontend Developer</h3>
             <h4>linuskiarie.website</h4>
 
-            <table border="0" cellSpacing="0" cellPadding="0">
-                <tbody>
-                    <tr>
-                        <td align="center" className='info--tabledata'>
-                            {/* <a rel="noopener" target="_blank" href="http://litmus.com">I am a button &rarr;</a> */}
-                            <a className='mail' href='mailto: kiarielinus@gmail.com'>Email</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <a className='mail' href='mailto: kiarielinus@gmail.com'>
+                    <img src={mail}/>
+                    Email    
+            </a>
 
         </div>)
 }
